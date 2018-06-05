b0d0b620964cb93f7070e47dd21709d1df2d9190
Lucas Vander Beken <lvb@teletask.be>
2018-06-05 22:52:56 +0200

    Adds a possible design for the profiles page

--- src/sections/profiles/ProfilesSection.js
+++ src/sections/profiles/ProfilesSection.js
@@ -6,22 +6,21 @@ class ProfilesSection extends Component {
         return (
             <section id="profiles">
                 <h1>Profiles.</h1>
-                <i className="socicon-facebook"></i>
-                <i className="socicon-twitter"></i>
-                <i className="socicon-strava"></i>
-                <i className="socicon-github"></i>
-                <i className="socicon-linkedin"></i>
-                <i className="socicon-imdb"></i>
+                <i className="profile socicon-github" />
+                <i className="profile socicon-stackoverflow" />
+                <i className="profile socicon-strava" />
+                <i className="profile socicon-linkedin" />
                 {/*https://www.imdb.com/user/ur25370628/ratings -->*/}
-                <i className="socicon-spotify"></i>
-                <i className="socicon-stackoverflow"></i>
-                <i className="socicon-medium"></i>
-                <i className="socicon-youtube"></i>
+                <i className="profile socicon-imdb" />
+                <i className="profile socicon-spotify" />
+                <i className="profile socicon-medium" />
 
-                <h2>Music I Love</h2>
-                <iframe src="https://open.spotify.com/embed/user/delucasvb/playlist/2nTcKrgAPT6YSgfhl4r9b1" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-                <iframe src="https://open.spotify.com/embed/user/delucasvb/playlist/0fudP8V92wVYwFDROl87ZH" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
-                <iframe src="https://open.spotify.com/embed/user/delucasvb/playlist/2JxtZtjRvYoPx6nxvWLVd6" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                <div className="container-fluid text-center">
+                    <h2>Music I Love</h2>
+                    <iframe src="https://open.spotify.com/embed/user/delucasvb/playlist/2nTcKrgAPT6YSgfhl4r9b1" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                    <iframe src="https://open.spotify.com/embed/user/delucasvb/playlist/0fudP8V92wVYwFDROl87ZH" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                    <iframe src="https://open.spotify.com/embed/user/delucasvb/playlist/2JxtZtjRvYoPx6nxvWLVd6" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                </div>
             </section>
         );
     }
