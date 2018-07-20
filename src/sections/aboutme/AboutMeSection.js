fc145dd22ad4007c3dc61d77960c5687abe6d84f
Lucas Vander Beken <lvb@teletask.be>
2018-07-20 14:15:11 +0200

    Fixes content for mobile screens

--- src/sections/aboutme/AboutMeSection.js
+++ src/sections/aboutme/AboutMeSection.js
@@ -1,55 +1,39 @@
 import React, {Component} from "react";
 import './aboutme.css'
 import faAddressCard from '@fortawesome/fontawesome-free-solid/faAddressCard'
-import faCogs from '@fortawesome/fontawesome-free-solid/faCogs'
-import faBinoculars from '@fortawesome/fontawesome-free-solid/faBinoculars'
 import faChess from '@fortawesome/fontawesome-free-solid/faChess'
+import faBinoculars from '@fortawesome/fontawesome-free-solid/faBinoculars'
 import ThoughtBubble from "../../components/ThoughtBubble/ThoughtBubble";
+import headCircle from "../../img/CV photo circle.png";
 
 class AboutMeSection extends Component {
     render() {
         return (
             <section id="about-me" className="section-aboutme">
+                <img id="head-circle" src={headCircle} alt="Headshot" />
                 <h1>About Me.</h1>
                 <div className="offset-md-1 col-md-5 text-white">
                     <ThoughtBubble icon={ faAddressCard } title="Some Personalia">
                         <p>
-                            Working on a product is my passion. I can push a dynamic team to its full potential, to
-                            deliver results beyond the expectation of the customer. Living technology day and night
-                            allows me to always bring something new and exciting to the drawing boards.
+                            Working on a product is my passion. My neurons are structured around research, discovery and
+                            delivery. Discovering a new challenge, and later a new solution, gives me my dopamine. I'm
+                            always curious.
                         </p>
                         <p>
                             <em>
-                                I'm 27 years young and living in Ghent, one of the historical, cultural and academic hotspots
-                                in Belgium.
+                                I'm 27 years young and living in Ghent, one of the historical, cultural and academic
+                                hotspots in Belgium.
                             </em>
                         </p>
                     </ThoughtBubble>
-                    <ThoughtBubble icon={ faCogs } title="A Bit About My Workflow">
-                        <p>
-                            Keeping things organised is a top priority for me. I use clear naming, versioning and
-                            thoughtful structures, and not just in code. The idea is to create a culture of quality that
-                            hopefully will propagate into the product itself.
-                        </p>
-                        <p>
-                            <em>
-                                Structure was very important when I was a disk jockey in college. Keeping my playlists
-                                neatly organised was key enabled me to keep up the pace in my performance.
-                            </em>
-                        </p>
-                        <p>
-                            Too many rules and you might loose your creativity though. Scrum, if applied well, can help
-                            overcome that risk. Aside from fixing the classic waterfall issue, of course.
-                        </p>
-                        <p>
-                            <em>
-                                As team lead in my bachelor's thesis, I coached my team to keep every one motivated,
-                                productive and most of all, happy.
-                            </em>
-                        </p>
+                    <ThoughtBubble icon={ faChess } title="A Bit About My Work">
                         <p>
                             Measure twice, cut once. I think before I do. Make diagrams, sketches, prototypes,...
-                            Whatever works to validate an idea before actually spending hours on a dead end.
+                            Whatever works to validate an idea before actually spending hours on a dead end. I am
+                            addicted to quality: in the things that I use in my daily live, in what I imagine , in what
+                            I create. Quality, in my belief,  can take many forms. It might be obvious, like in the
+                            design and durability of an Italian shoe, or less so, like in the history and pride of our
+                            own Belgian fries.
                         </p>
                         <p>
                             <em>
@@ -59,26 +43,12 @@ class AboutMeSection extends Component {
                             </em>
                         </p>
                     </ThoughtBubble>
-                    <ThoughtBubble icon={ faChess } title="What I Can Do">
-                        <p>
-                            Specialization is not my speciality. As soon as I have a thorough understanding of how
-                            something works, I try to get a grasp of the bigger picture. Are there competing frameworks
-                            or other philosophies for example. What alternatives are there?
-                        </p>
-                        <p>
-                            <em>
-                                I'm schooled in Java and experienced in Spring. But for my personal projects, I always
-                                try to use something different. This helps me keep a wide overview during the design or
-                                research phase. Right before implementation starts, is the perfect time to dive deeper.
-                            </em>
-                        </p>
-                    </ThoughtBubble>
                     <ThoughtBubble icon={ faBinoculars } title="What I'm Looking For">
                         <p>
                             I want to be part of a dynamic team, in a company with ambition and a clear vision. Being on
-                            the same line strategically is very important to me. I need to be able to believe in the
-                            product. I'm eager to learn from experienced people and men and women with a different
-                            background, culture or vision.
+                            the same line strategically and philosophically is very important to me. I need to be able
+                            to believe in the idea and the product. I'm eager to learn from experienced people and men
+                            and women with a different background, culture or vision.
                         </p>
                     </ThoughtBubble>
                 </div>
--- src/sections/profiles/ProfilesSection.js
+++ src/sections/profiles/ProfilesSection.js
@@ -12,11 +12,14 @@ class ProfilesSection extends Component {
                 <a href="https://github.com/delucasvb" className="deco-none">
                     <i className="profile socicon-github" />
                 </a>
-                <a href="https://www.strava.com/athletes/20132720" className="deco-none">
-                    <i className="profile socicon-strava" />
-                </a>
                 <a href="https://www.linkedin.com/in/lucas-vander-beken-67429348/" className="deco-none">
                     <i className="profile socicon-linkedin" />
+                </a>
+                <a href="https://medium.com/@delucasvb" className="deco-none">
+                    <i className="profile socicon-medium" />
+                </a>
+                <a href="https://www.strava.com/athletes/20132720" className="deco-none">
+                    <i className="profile socicon-strava" />
                 </a>
                 <a href="https://www.imdb.com/user/ur25370628/ratings" className="deco-none">
                     <i className="profile socicon-imdb" />
@@ -24,21 +27,18 @@ class ProfilesSection extends Component {
                 <a href="https://open.spotify.com/user/delucasvb" className="deco-none">
                     <i className="profile socicon-spotify" />
                 </a>
-                <a href="https://medium.com/@delucasvb" className="deco-none">
-                    <i className="profile socicon-medium" />
-                </a>
 
                 <div className="container-fluid text-center">
                     <h2>Music I Love</h2>
                     <div className="row">
                         <div className="col-md-4">
-                            <iframe src="https://open.spotify.com/embed/user/delucasvb/playlist/2nTcKrgAPT6YSgfhl4r9b1" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                            <iframe src="https://open.spotify.com/embed/user/delucasvb/playlist/2nTcKrgAPT6YSgfhl4r9b1" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media" title="Living Room Party"></iframe>
                         </div>
                         <div className="col-md-4">
-                            <iframe src="https://open.spotify.com/embed/user/delucasvb/playlist/0fudP8V92wVYwFDROl87ZH" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                            <iframe src="https://open.spotify.com/embed/user/delucasvb/playlist/0fudP8V92wVYwFDROl87ZH" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media" title="School of Rock"></iframe>
                         </div>
                         <div className="col-md-4">
-                            <iframe src="https://open.spotify.com/embed/user/delucasvb/playlist/2JxtZtjRvYoPx6nxvWLVd6" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+                            <iframe src="https://open.spotify.com/embed/user/delucasvb/playlist/2JxtZtjRvYoPx6nxvWLVd6" width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media" title="Sweaty"></iframe>
                         </div>
                     </div>
                 </div>
